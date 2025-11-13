d1aacfbdf58ec271b813b6f90f55afa3d43e090a
yash_warhekar <yash2000warhekar@gmail.com>
2025-11-14 00:35:32 +0530

    basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Navbar from "./Navbar"
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
+
 
 function App() {
 
   return (
     <>
-        <Navbar/>
-       <button class="btn w-64 rounded-full">Button</button>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
